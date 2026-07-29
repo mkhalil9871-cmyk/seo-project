58919dd6a935b0d298b8b712a84ec54e6afb5f60
semp <ah8885434@gmail.com>
2026-07-29 12:03:19 +0500

    Add Page and Audit models, migrations, and relationships

--- backend/app/Models/Project.php
+++ backend/app/Models/Project.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    
     use HasFactory;
 
     protected $fillable = ['user_id', 'name', 'domain', 'industry', 'country', 'language', 'status'];
@@ -14,5 +15,10 @@ class Project extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function pages()
+    {
+        return $this->hasMany(Page::class);
     }
 }
